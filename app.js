93ed6ff91ec48aadb6b03979861d5a75631863d8
Tom Slinger <t.slinger@outlook.com>
2018-04-03 16:34:08 +0100

    added hover enter and leave event listeners

--- app.js
+++ app.js
@@ -2,3 +2,18 @@
 const left = document.querySelector('left');
 const right = document.querySelector('right');
 const container = document.querySelector('container');
+
+// Event Listeners
+left.addEventListener('mouseenter', () => {
+    container.classList.add('hover-left');
+});
+left.addEventListener('mouseleave', () => {
+    container.classList.remove('hover-left');
+});
+
+right.addEventListener('mouseenter', () => {
+    container.classList.add('hover-right');
+});
+right.addEventListener('mouseleave', () => {
+    container.classList.remove('hover-right');
+});
