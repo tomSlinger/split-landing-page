de4ac1ea0bb72cf1c24aa08ae315c270c4fa2ca3
Tom Slinger <t.slinger@outlook.com>
2018-04-03 21:05:17 +0100

    added event listeners to right side

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 
+// Event Listeners
 left.addEventListener('mouseenter', () => {
     left.classList.add('hover');
     right.classList.add('opacity');
@@ -11,3 +12,13 @@ left.addEventListener('mouseleave', () => {
     left.classList.remove('hover');
     right.classList.remove('opacity');
 });
+
+right.addEventListener('mouseenter', () => {
+    right.classList.add('hover');
+    left.classList.add('opacity');
+});
+
+right.addEventListener('mouseleave', () => {
+    right.classList.remove('hover');
+    left.classList.remove('opacity');
+})
